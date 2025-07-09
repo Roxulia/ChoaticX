d4b23bf2eadfc1233eaebbd2fbe11557c929261d
Roxulia <122478073+Roxulia@users.noreply.github.com>
2025-07-09 19:32:29 +0630

    Update timeFrames.py

--- Data/timeFrames.py
+++ Data/timeFrames.py
@@ -1,4 +1,23 @@
+from binance.client import Client
+class timeFrame:
+    def __init__(self):
+        self.tf = {
+            '1min' : Client.KLINE_INTERVAL_1MINUTE,
+            '3min' :Client.KLINE_INTERVAL_3MINUTE,
+            '5min' : Client.KLINE_INTERVAL_5MINUTE,
+            '15min' : Client.KLINE_INTERVAL_15MINUTE,
+            '1h' : Client.KLINE_INTERVAL_1HOUR,
+            '4h' : Client.KLINE_INTERVAL_4HOUR,
+            '1D' : Client.KLINE_INTERVAL_1DAY
+        }
+        self.tfOrder = ['1min','3min','5min','15min','1h','4h','1D']
 
+    def getTimeFrame(self,interval):
+        return self.tf[interval]
+    
+    def getTFOrder(self,interval):
+        return self.tfOrder.index(interval)
+    
     def getSmallestTF(self,all_df):
         if not all_df:
             return []
