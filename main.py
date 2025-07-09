847ac4f5c8a56c6a164f5e5701b5c2f0f9e36b00
Roxulia <122478073+Roxulia@users.noreply.github.com>
2025-07-09 12:52:32 +0630

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from Core.zone_detection import ZoneDetector
+from Core.zone_reactions import ZoneReactor
+from Core.zone_merge import ZoneMerger
+from Core.SignalGeneration import SignalGenerator
+from Core.Filter import Filter
+from ML.Model import Model
+from ML.transform import Transformer
+from Data.binanceAPI import BinanceAPI
+
+class SignalService:
+    def __init__(self):
+        self.api = BinanceAPI()
+        #self.model = Model(...)  # Load pretrained model and transformer
+        self.signal_gen = SignalGenerator(models={'entry_model': self.model})
+
+    def get_latest_zones(self):
+        df = self.api.get_ohlcv(interval= '1h')
+        detector = ZoneDetector(df)
+        zones_1h = detector.get_zones()
+        df = self.api.get_ohlcv(interval= '4h')
+        detector = ZoneDetector(df,'4h')
+        zones_4h = detector.get_zones()
+        df = self.api.get_ohlcv(interval= '1D')
+        detector = ZoneDetector(df,'1D')
+        zones_1D = detector.get_zones()
+        merger = ZoneMerger(zones_1h+zones_4h+zones_1D)
+        zones = merger.merge()
+        zones = merger.add_liq_confluence(zones)
+        return zones
+
+    def get_current_signals(self):
+        df = self.api.get_ohlcv()
+        zones = self.get_latest_zones()
+        reactor = ZoneReactor(df, zones)
+        reaction = reactor.get_last_candle_reaction()
+
+        signals = []
+        for zone, reaction in zip(zones, reaction):
+            signal = self.signal_gen.generate(df, zone, reaction)
+            if signal:
+                signals.append(signal)
+        return signals
