7c2e2d183c92ed342a38b85a87862a1794a74442
Roxulia <122478073+Roxulia@users.noreply.github.com>
2025-07-09 22:41:42 +0630

    Update timeFrames.py

--- Data/timeFrames.py
+++ Data/timeFrames.py
@@ -11,6 +11,44 @@ class timeFrame:
             '1D' : Client.KLINE_INTERVAL_1DAY
         }
         self.tfOrder = ['1min','3min','5min','15min','1h','4h','1D']
+        self.multiplier = {
+            '1min' : {
+                '1min' : 1,
+                '3min': 3,
+                '5min' : 5,
+                '15min' : 15,
+                '1h' : 60,
+                '4h' : 240,
+                '1D' : 1440
+            },
+            '3min':{
+                '3min' : 1,
+                '5min' : 2,
+                '15min' : 5,
+                '1h' : 15,
+                '4h' : 60,
+                '1D' : 240
+            },
+            '5min' : {
+                '5min' : 1,
+                '15min' : 3,
+                '1h' : 12,
+                '4h' : 48,
+                '1D' : 288
+            },
+            '1h' : {
+                '1h' : 1,
+                '4h' : 4,
+                '1D' : 24
+            },
+            '4h' : {
+                '4h' : 1,
+                '1D' : 6
+            },
+            '1D' : {
+                '1D' : 1
+            }
+        }
 
     def getTimeFrame(self,interval):
         return self.tf[interval]
@@ -24,13 +62,16 @@ class timeFrame:
 
         # Get the minimum order value (i.e. smallest timeframe)
         min_order = min(self.getTFOrder(z['time_frame']) for z in all_df if 'time_frame' in z)
-        return min_order
+        return self.tfOrder[min_order]
     
     def getBasedZone(self,all_df):
         if not all_df:
             return []
-
+        min_order = min(self.getTFOrder(z['time_frame']) for z in all_df if 'time_frame' in z)
         # Filter only zones with that smallest timeframe
-        smallest_zones = [z for z in all_df if self.getTFOrder(z['time_frame']) == self.getSmallestTF(all_df)]
+        smallest_zones = [z for z in all_df if self.getTFOrder(z['time_frame']) == min_order]
         return smallest_zones
+    
+    def getMultiplier(self,smallest,current):
+        return self.multiplier[smallest][current]
     
