7229e75c3bd7f0cb42325d81a9361c59ef4c4ec8
Roxulia <122478073+Roxulia@users.noreply.github.com>
2025-07-12 21:25:38 +0630

    Update transform.py

--- ML/transform.py
+++ ML/transform.py
@@ -1,11 +1,15 @@
 from sklearn.preprocessing import StandardScaler
 
-class Transformer:
+class DataTransformer:
     def __init__(self):
         self.scaler = StandardScaler()
 
-    def fit_transform(self, X):
-        return self.scaler.fit_transform(X)
+    def fit_transform(self, df):
+        X = df.drop('label', axis=1)
+        y = df['label']
+        X_scaled = self.scaler.fit_transform(X)
+        return X_scaled, y
 
-    def transform(self, X):
+    def transform(self, df):
+        X = df.drop('label', axis=1)
         return self.scaler.transform(X)
