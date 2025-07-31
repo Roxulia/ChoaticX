90b776731a3e931c6bb9512316b5000154152cb1
Roxulia <122478073+Roxulia@users.noreply.github.com>
2025-07-31 10:24:33 +0630

    Update indexCalculate.py
    Add calculation method

--- Data/indexCalculate.py
+++ Data/indexCalculate.py
@@ -7,4 +7,20 @@ class IndexCalculator():
         self.timeframe = timeFrame()
 
     def calculate(self):
-        
+        smallest_tf = tf.getSmallestTF(self.zones)
+        for zone in self.zones:
+            if zone['type'] in ['Buy-Side Liq','Sell-Side Liq']:
+                if zone.get('index') is not None:
+                    zone['index'] = zone['index'] * tf.getMultiplier(smallest_tf,zone['time_frame'])
+                if zone.get('swept_index') is not None:
+                    zone['swept_index'] = zone['swept_index'] * tf.getMultiplier(smallest_tf,zone['time_frame'])
+                if zone.get('end_index') is not None:
+                    zone['end_index'] = zone['end_index'] * tf.getMultiplier(smallest_tf,zone['time_frame'])
+                if zone.get('touch_indexs') is not None:
+                    zone['touch_indexs'] = [i * tf.getMultiplier(smallest_tf,zone['time_frame']) for i in zone['touch_indexs'] if i is not None]
+            else :
+                if zone.get('index') is not None:
+                    zone['index'] = zone['index'] * tf.getMultiplier(smallest_tf,zone['time_frame'])
+                if zone.get('touch_index') is not None:
+                    zone['touch_index'] = zone['touch_index'] * tf.getMultiplier(smallest_tf,zone['time_frame'])
+        return self.zones
