45a69f739fc8b59f40c91075d9f4cadea8f65ba9
Zin Lin Htet <122478073+Roxulia@users.noreply.github.com>
2025-09-17 20:27:58 +0630

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ scheduler.start()
 app.register_blueprint(signal_api.blueprint, url_prefix="/api")
 
 if __name__ == "__main__":
-    socketio.run(app,debug=True)
+    socketio.run(app,host = "0.0.0.0",port = 5000)
