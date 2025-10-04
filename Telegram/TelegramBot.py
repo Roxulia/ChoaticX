b43c905f405b63930f755ba242c6515654e6cef2
Roxulia <122478073+Roxulia@users.noreply.github.com>
2025-10-04 16:36:48 +0630

    Added seperate command for BNB token in telegrma bot

--- Telegram/TelegramBot.py
+++ Telegram/TelegramBot.py
@@ -1,4 +1,5 @@
 from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import *
 import os
 from dotenv import load_dotenv
@@ -10,25 +11,101 @@ import logging
 from logging.handlers import RotatingFileHandler
 from Database.DataModels.Subscribers import Subscribers
 from Backtest.Portfolio import *
+from functools import wraps
 
 class TelegramBot:
     def __init__(self, service : SignalService):
         load_dotenv()
         self.CAPITAL_UPDATE = 1
-        self.TELEGRAM_TOKEN = os.getenv("BOT_API")
+        self.TELEGRAM_TOKEN = os.getenv("TEST_BOT_API")
         self.btcservice = SignalService("BTCUSDT",300)
+        self.bnbservice = SignalService("BNBUSDT",threshold=3)
         self.app = Application.builder().token(self.TELEGRAM_TOKEN).post_init(self.post_init).build()
         self.redis = redis.Redis(host = '127.0.0.1',port = 6379,db=0)
         self.pubsub = self.redis.pubsub()
         self.pubsub.subscribe("signals_channel")
         
+    def restricted(min_tier=1, admin_only=False,for_starter = False):
+        def decorator(func):
+            @wraps(func)
+            async def wrapper(self,update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs):
+                user_id = update.effective_user.id
+                user = Subscribers.getByChatID(user_id)
+                if not for_starter:
+                    
+                    if not user:
+                        await update.message.reply_text("❌ You are not registered.")
+                        return
+
+                    if admin_only and not user['is_admin']:
+                        await update.message.reply_text("🚫 Admins only.")
+                        return
+
+                    if not user['is_admin'] and user['tier'] < min_tier:
+                        await update.message.reply_text(
+                            f"⚠️ This command requires *Tier {min_tier}* or higher. Please upgrade your subscription.",
+                            parse_mode="Markdown"
+                        )
+                        return
+                return await func(self,update, context,user, *args, **kwargs)
+            return wrapper
+        return decorator
+    
     async def post_init(self, app: Application):
         # Start Redis listener as background task once loop is running
         app.create_task(self.listener())
         
     # ---------------- Handlers ----------------
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text("Hey! I’m your ChaoticX bot. Try /zones to see latest zone formation.\n Type /subscribe to enable bot to send u signal everytime it finds a signal")
+        keyboard = [
+            [InlineKeyboardButton("📊 View BTCUSDT Zones", callback_data="btc_zones")],
+            [InlineKeyboardButton("📈 View BTCUSDT Signals", callback_data="btc_signals")],
+            [InlineKeyboardButton("🔔 Subscribe for Signals", callback_data="subscribe")],
+            [InlineKeyboardButton("ℹ️ Help", callback_data="help")]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        welcome_text = (
+            "👋 *Welcome to ChaoticX Bot!*\n\n"
+            "I’m your trading assistant for detecting smart money concepts 📈.\n\n"
+            "✨ What I can do for you:\n"
+            "• `/btc_zones` → Show latest zone formations (FVGs, OBs, Liquidity)\n"
+            "• `/btc_signals` → Show latest BTC signal informations\n"
+            "• `/subscribe` → Get real-time signals when new setups appear\n"
+            "• `/help` → Learn how to use me\n\n"
+            "⚡ Let’s start trading smarter!"
+        )
+
+        await update.message.reply_text(
+            welcome_text,
+            reply_markup=reply_markup,
+            parse_mode="Markdown"
+        )
+
+    @restricted(for_starter=True)  # all registered users can use help
+    async def help(self, update: Update, context: ContextTypes.DEFAULT_TYPE, user):
+        keyboard = [
+            [InlineKeyboardButton("📊 BTCUSDT Zones", callback_data="btc_zones")],
+            [InlineKeyboardButton("🔔 Subscribe", callback_data="subscribe")],
+            [InlineKeyboardButton("💰 Update Capital", callback_data="update_capital")] if user is not None and (user['tier']>1 or user['is_admin']) else None,
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        if user is None:
+            tier = 0
+        else:
+            tier = user['tier']
+        help_text = (
+            "🤖 *ChaoticX Bot Help*\n\n"
+            "Here’s what I can do for you:\n\n"
+            "• `/btc_zones` → Show the latest zone formations (FVGs, OBs, Liquidity).\n"
+            "• `/subscribe` → Subscribe to real-time signals when setups appear and increase ur tier to 1.\n"
+            "• `/update_capital` → Update your portfolio capital size.\n"
+            "• `/cancel` → Cancel an ongoing action (like capital update).\n\n"
+            "⚡ *Your Tier:* {tier}\n"
+            "Use the buttons below for quick access 👇"
+        ).format(tier=tier)
+
+        await update.message.reply_text(help_text, reply_markup=reply_markup, parse_mode="Markdown")
 
     async def subscribe(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id = update.effective_chat.id
@@ -58,11 +135,12 @@ class TelegramBot:
             print("Error in Database")
             await update.message.reply_text("Unknown Error Occur !! Pls Contact Us for Support")
 
-    async def get_zones(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    @restricted(for_starter=True)
+    async def get_btc_zones(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         try:
             zones = self.btcservice.get_untouched_zones(limit= 5)
             sorted_zones = sorted(zones, key=lambda x: x.get("timestamp"),reverse= True)[:4]
-            msg = "Recent Zones\n"
+            msg = "Recent BTC Zones\n"
             for zz in sorted_zones:
                 msg = msg +  f"Zone Type : {zz['zone_type']},Zone High: {zz['zone_high']}, Low: {zz['zone_low']}, Time: {zz['timestamp']}\n"
             
@@ -72,56 +150,91 @@ class TelegramBot:
         except Exception as e:
             await update.message.reply_text(f"Error: {str(e)}")
 
-    async def get_running_signals(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    @restricted(for_starter=True)
+    async def get_given_btc_signals(self, update: Update, context: ContextTypes.DEFAULT_TYPE,user):
         try:
-            signals = self.btcservice.get_running_signals()
-            msg = "Recent Running Signals\n"
-            for s in signals:
-                msg = msg +  f"Signal Side: {s['position']} | Symbol: {s['symbol']} | Entry: {s['entry_price']} | TP: {s['tp']} | SL: {s['sl']}\n"
+            signals = self.btcservice.get_given_signals()
+            msg = "Recent BTCUSDT Signals\n"
+            if user is not None and (user['tier'] > 1 or user['is_admin']):
+                for s in signals:
+                    porfolio = Portfolio(starting_balance= user['capital'])
+                    lot_size = porfolio.risk_position_size(s['entry_price'],s['sl'],s['risk_size'])
+                    msg = msg +  f"Signal Side: {s['position']} | Symbol: {s['symbol']} | Entry: {s['entry_price']} | TP: {s['tp']} | SL: {s['sl']} | Lot Size: {lot_size}\n"
+            else:
+                for s in signals:
+                    msg = msg +  f"Signal Side: {s['position']} | Symbol: {s['symbol']} | Entry: {s['entry_price']} | TP: {s['tp']} | SL: {s['sl']}\n"
             await update.message.reply_text(msg)
         except EmptySignalException as e:
             await update.message.reply_text(str(e))
         except Exception as e:
             await update.message.reply_text(f"Error: {str(e)}")
 
-    async def update_subscriber_capital(self,update:Update,context:ContextTypes.DEFAULT_TYPE):
+    @restricted(min_tier=2)
+    async def get_bnb_zones(self,update:Update,context:ContextTypes.DEFAULT_TYPE):
         try:
-            user_id = update.effective_user.id
-            user = Subscribers.getByChatID(user_id)
-            if user['tier'] > 1 or user['is_admin'] :
-                await update.message.reply_text("Please enter your new capital size in USDT or 'cancel' to cancel updating")
-                return self.CAPITAL_UPDATE
-            else:
-                await update.message.reply_text(f"❌ Your Account Tier Too Low to Update Capital")
-                return ConversationHandler.END
+            zones = self.bnbservice.get_untouched_zones(limit= 5)
+            sorted_zones = sorted(zones, key=lambda x: x.get("timestamp"),reverse= True)[:4]
+            msg = "Recent BNBUSDT Zones\n"
+            for zz in sorted_zones:
+                msg = msg +  f"Zone Type : {zz['zone_type']},Zone High: {zz['zone_high']}, Low: {zz['zone_low']}, Time: {zz['timestamp']}\n"
+            
+            await update.message.reply_text(msg)
+        except NoUntouchedZone as e:
+            await update.message.reply_text(str(e))
         except Exception as e:
-            await update.message.reply_text("Error Occured")
+            await update.message.reply_text(f"Error: {str(e)}")
 
-    async def set_capital(self,update: Update, context: ContextTypes.DEFAULT_TYPE):
+    @restricted(min_tier=2)
+    async def get_given_bnb_signals(self, update: Update, context: ContextTypes.DEFAULT_TYPE,user):
+        try:
+            signals = self.bnbservice.get_given_signals()
+            msg = "Recent BNBUSDT Signals\n"
+            for s in signals:
+                porfolio = Portfolio(starting_balance= user['capital'])
+                lot_size = porfolio.risk_position_size(s['entry_price'],s['sl'],s['risk_size'])
+                msg = msg +  f"Signal Side: {s['position']} | Symbol: {s['symbol']} | Entry: {s['entry_price']} | TP: {s['tp']} | SL: {s['sl']} | Lot Size: {lot_size}\n"
+            await update.message.reply_text(msg)
+        except EmptySignalException as e:
+            await update.message.reply_text(str(e))
+        except Exception as e:
+            await update.message.reply_text(f"Error: {str(e)}")
+
+    @restricted(min_tier=2)  # only Tier ≥2 or admins
+    async def update_subscriber_capital(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        try:
+            await update.message.reply_text(
+                "💰 Please enter your new capital size in *USDT* or type 'cancel' to stop:",
+                parse_mode="Markdown"
+            )
+            return self.CAPITAL_UPDATE
+        except Exception as e:
+            await update.message.reply_text("⚠️ An error occurred while starting update.")
+            return ConversationHandler.END
+
+    @restricted(min_tier=2)  # only Tier ≥2 or admins
+    async def set_capital(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         try:
             capital = float(update.message.text)
             if capital <= 0:
-                await update.message.reply_text("Capital must be greater than 0. Try again:")
+                await update.message.reply_text("⚠️ Capital must be greater than 0. Try again:")
                 return self.CAPITAL_UPDATE
 
-            # Example: update in DB
+            # ✅ Update in DB
             user_id = update.effective_user.id
             user = Subscribers.getByChatID(user_id)
-            if user['tier'] > 1 or user['is_admin'] :
-                Subscribers.update(user['id'],{"capital":capital})
-                await update.message.reply_text(f"✅ Your capital has been updated to {capital} USDT.")
-            else:
-                await update.message.reply_text(f"❌ Your Account Tier Too Low to Update Capital")
-            
+            Subscribers.update(user['id'], {"capital": capital})
+
+            await update.message.reply_text(f"✅ Your capital has been updated to *{capital} USDT*.", parse_mode="Markdown")
             return ConversationHandler.END
 
         except ValueError:
             await update.message.reply_text("❌ Please enter a valid number:")
             return self.CAPITAL_UPDATE
 
-    async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text("Capital update canceled.")
+    async def cancel(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("❌ Capital update canceled.")
         return ConversationHandler.END
+    
 
     # ---------------- Broadcast ----------------
     async def broadcast_signals(self, signal):
@@ -165,7 +278,7 @@ class TelegramBot:
     def run(self):
         # Register bot handlers
         capital_update_handler = ConversationHandler(
-        entry_points=[CommandHandler("update-capital", self.update_subscriber_capital)],
+        entry_points=[CommandHandler("update_capital", self.update_subscriber_capital)],
         states={
             self.CAPITAL_UPDATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.set_capital)],
         },
@@ -174,8 +287,11 @@ class TelegramBot:
         self.app.add_handler(CommandHandler("start", self.start))
         self.app.add_handler(CommandHandler("subscribe", self.subscribe))
         self.app.add_handler(CommandHandler("unsubscribe", self.unsubscribe))
-        self.app.add_handler(CommandHandler("zones", self.get_zones))
-        self.app.add_handler(CommandHandler("signals", self.get_running_signals))
+        self.app.add_handler(CommandHandler("btc_zones", self.get_btc_zones))
+        self.app.add_handler(CommandHandler("btc_signals", self.get_given_btc_signals))
+        self.app.add_handler(CommandHandler("bnb_zones", self.get_bnb_zones))
+        self.app.add_handler(CommandHandler("bnb_signals", self.get_given_bnb_signals))
+        self.app.add_handler(CommandHandler("help", self.help))
         self.app.add_handler(capital_update_handler)
         self.app.run_polling()
 
