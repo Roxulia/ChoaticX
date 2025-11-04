4ca8b988618d8afa70c46ad7cd50901ea3bf7d77
Roxulia <122478073+Roxulia@users.noreply.github.com>
2025-11-04 21:47:24 +0630

    add new core class

---
+++ Core/MA_related_zones.py
@@ -0,0 +1,12 @@
+class MovingAverageCrossOver():
+    def __init__(self):
+        pass
+
+    def detect(self):
+        pass
+
+    def getPreviousCrossOver(self):
+        pass
+
+    def updatePreviousCrossOver(self):
+        pass
